a21e89963f560b596f45030e757e8100a865a32e
Paserno <paserno.x@gmail.com>
2024-04-30 16:08:17 -0400

    creacion de nuevas instancias al loggear

--- utility/discord_bot.js
+++ utility/discord_bot.js
@@ -7,10 +7,16 @@ const { COMMANDS } = require('../constants/general');
 
 
 // const { Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, PermissionsBitField, Permissions, AttachmentBuilder } = require('discord.js');
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
-});
+// let client = new Client({
+//     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+// });
+let client = null;
 
+const createClient = () => {
+    return new Client({
+        intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+    });
+}
 
 
 const handleReadyEvent = (x) => {
@@ -54,13 +60,22 @@ const handleReadyEvent = (x) => {
 
 
 //ejecuciones iniciales del bot, inicializacion de los comandos
-client.on('ready', (x) => handleReadyEvent(x));
+// client.on('ready', (x) => handleReadyEvent(x));
+
+const initializeClient = () => {
+    if (!client) {
+        client = createClient();
+        client.on('ready', handleReadyEvent);
+        client.on('interactionCreate', handleInteraction);
+    }
+}
 
 
 
 
 //funciones de los comandos creados anteriormente
-client.on('interactionCreate', async (interaction) => {
+// client.on('interactionCreate', async (interaction) => {
+const handleInteraction = async (interaction) => {
     /** comando de ping, al utilizando, devuelve un mensaje escrito basico, con la info de cantidad de jugadores online y sus nicks
      *  */
     if (!interaction.isChatInputCommand()) return;
@@ -77,10 +92,14 @@ client.on('interactionCreate', async (interaction) => {
         default: return;
     }
     //comando de marco, devuelve un mensaje formato embed, el cual posee una mejor estructura del mensaje
-});
+};
 
 
-const loginClient = () => client.login(process.env.TOKEN);
+// const loginClient = () => client.login(process.env.TOKEN);
+const loginClient = async () => {
+    initializeClient();
+    return await client.login(process.env.TOKEN);
+};
 
 
 
@@ -89,12 +108,14 @@ const destruirCliente = () => {
         client.destroy()
             .then(() => console.log('Cliente Discord destruido exitosamente'))
             .catch(error => console.error('Error al destruir el cliente Discord:', error));
+        client = null;
     } else {
         console.error('El cliente Discord no está definido.');
     }
 }
 
 const revisionStatus = () => {
+    if (!client) return false;
     const destroyed = client.rest.hashTimer._destroyed;
     const webSocket = client.ws._ws;
     // console.log(client);
